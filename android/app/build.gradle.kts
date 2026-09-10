70c289f40a5b55f09e65dd7e2287d19491199aaf
Sujal Singh <davasnsujal@gmail.com>
2026-09-11 02:46:55 +0530

    fix: update Android API 36 and dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "com.findon.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
-        targetSdk = flutter.targetSdkVersion
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
